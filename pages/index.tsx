6d048ee5285a2fc8fecaf5f1d05b9c949e53a175
José Andersson <jamfreire@gmail.com>
2022-10-31 18:47:46 -0300

    Primeira história

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Button from "../src/compoents/Button/Button";
+
 export default function HomeScreen() {
   return (
     <div>
       <h1>Home Page!</h1>
+      <Button>Botão</Button>
     </div>
   );
 }
