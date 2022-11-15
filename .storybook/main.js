5d7e590d9374a9c5b541c640a58430335eac2f11
José Andersson <jamfreire@gmail.com>
2022-11-15 09:59:44 -0300

    Customizando o Storybook (temas e addons)

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "addon-screen-reader",
   ],
   framework: "@storybook/react",
   core: {
